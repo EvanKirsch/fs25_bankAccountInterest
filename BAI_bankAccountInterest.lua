d400494c4d17a9dd212f646bd505b62f2427c7be
Evan Kirsch <kirsch.j.evan@gmail.com>
2025-12-04 21:50:09 -0600

    inital commit

---
+++ BAI_bankAccountInterest.lua
@@ -0,0 +1,69 @@
+-- BAI_bankAccountInterest
+-- Main driver class for BAI
+--
+
+BAI_bankAccountInterest = {}
+BAI_bankAccountInterest.rate = 0.03
+BAI_bankAccountInterest.commands = {
+    --- call name, description, function
+    { 'biSetInterestRate', 'sets annual bank account interest rate', 'setInterestRate' },
+    { 'biGetInterestRate', 'gets annual bank account interest rate', 'getInterestRate' },
+}
+
+local BAI_bankAccountInterest_mt = Class(BAI_bankAccountInterest, AbstractManager)
+
+function BAI_bankAccountInterest.new(customMt)
+    local self = BAI_bankAccountInterest:superClass().new(customMt or DCE_devConsoleExtended_mt)
+
+    return self
+end
+
+function BAI_bankAccountInterest:loadMap()
+    g_messageCenter:subscribe(MessageType.PERIOD_CHANGED, self.onPeriodChanged, self)
+end
+
+function BAI_bankAccountInterest:onPeriodChanged()
+    if g_currentMission:getIsServer() then
+        local farms = g_farmManager.farmIdToFarm
+        for _, farm in pairs(farms) do
+            cashAmount = farm.money
+            farmId = farm.farmId
+
+            if cashAmount ~= nil and cashAmount > 0 then
+                local interestAmount = cashAmount * (BAI_bankAccountInterest.rate / 12)
+                g_currentMission:addMoney(interestAmount, farmId, MoneyType.OTHER, true, true)
+            end
+
+        end
+
+    end
+end
+
+
+-- Borrowed from Courseplay
+function BAI_bankAccountInterest:init()
+    self:registerConsoleCommands()
+end
+
+-- Borrowed from Courseplay
+function BAI_bankAccountInterest:registerConsoleCommands()
+    for _, commandData in ipairs(self.commands) do
+        local name, desc, funcName = unpack(commandData)
+        addConsoleCommand(name, desc, funcName, self)
+    end
+end
+
+---@param myRate rate to be set, expressed as float
+function BAI_bankAccountInterest:setInterestRate(myRate)
+    BAI_bankAccountInterest.rate = myRate
+    print("BAI_bankAccountInterest.rate=" .. tostring(BAI_bankAccountInterest.rate))
+    return myString
+end
+
+function BAI_bankAccountInterest:getInterestRate()
+    print("BAI_bankAccountInterest.rate=" .. tostring(BAI_bankAccountInterest.rate))
+    return BAI_bankAccountInterest.rate
+end
+
+BAI_bankAccountInterest:init()
+addModEventListener(BAI_bankAccountInterest)
